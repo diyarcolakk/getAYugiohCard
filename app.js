e58e61481a3e5e764f7a8e7ef900121641101835
diyarcolakk <114312886+diyarcolakk@users.noreply.github.com>
2022-12-27 19:12:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+// const yuGiOh = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+
+
+
+const yugiOhApiUse = function () {
+    // const a = fetch(`${yuGiOh}`).then(respond => respond.json()).then(cardinfo => console.log(cardinfo.data))
+}
+
+
+yugiOhApiUse();
