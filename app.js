6e57898c08b05285a37f18cf29c164bf07fc6fcd
diyarcolakk <114312886+diyarcolakk@users.noreply.github.com>
2023-01-06 02:43:38 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
+const yuGiOh = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
 
-
-
-// const yuGiOh = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
-
-
+const imgUrlBul = function (cardinfo) {
+  let array = [];
+  for (let i = 0; i < 10; i++) {
+    array[i] = cardinfo.data[i].card_images[0].image_url_small;
+  }
+  return array;
+};
 
 const yugiOhApiUse = function () {
-    // const a = fetch(`${yuGiOh}`).then(respond => respond.json()).then(cardinfo => console.log(cardinfo.data))
-}
+  fetch(`${yuGiOh}`)
+    .then((respond) => respond.json())
+    .then((cardinfo) => {
+      if (imgUrlBul(cardinfo))
+        return imgUrlBul(cardinfo); // Array geldi. Bu arrayi divde yerine koy
+      else throw new Error("Card image ulaşılamadı");
+    })
+    .catch((err) => console.warn(err));
+};
 
+//Fotoğraflara erişmek için bir yol
 
-yugiOhApiUse();
+// const yugiOhApiUse = function () {
+//     const a = fetch(`${yuGiOh}`)
+//       .then((respond) => respond.json())
+//       .then((cardinfo) => {
+//         let array = [];
+//         for (let i = 0; i < 10; i++) {
+//           array[i] = cardinfo.data[i].card_images;
+//         }
+//         return array;
+//       })
+//       .then((img) => console.log(img[1][0].image_url));
+//   };
+
+//   yugiOhApiUse();
