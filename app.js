a63f547c00d4124eb6efbe9cea372fbe98f48bf4
diyarcolakk <114312886+diyarcolakk@users.noreply.github.com>
2023-01-06 03:03:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const yuGiOh = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
-
+const cardContainerShow = document.querySelector(".card-container");
 const imgUrlBul = function (cardinfo) {
   let array = [];
+  let idArr = [];
   for (let i = 0; i < 10; i++) {
-    array[i] = cardinfo.data[i].card_images[0].image_url_small;
+    array[i] = cardinfo.data[i].card_images[0].image_url;
   }
+
   return array;
 };
 
@@ -12,12 +14,31 @@ const yugiOhApiUse = function () {
   fetch(`${yuGiOh}`)
     .then((respond) => respond.json())
     .then((cardinfo) => {
-      if (imgUrlBul(cardinfo))
-        return imgUrlBul(cardinfo); // Array geldi. Bu arrayi divde yerine koy
+      if (imgUrlBul(cardinfo)) yerlestirme(imgUrlBul(cardinfo));
+      // Array geldi. Bu arrayi divde yerine koy
       else throw new Error("Card image ulaşılamadı");
     })
     .catch((err) => console.warn(err));
 };
+
+function yerlestirme(gelenimgUrl) {
+  let html = "";
+  for (let i = 0; i < gelenimgUrl.length; i++) {
+    html += `<div class="card" style="margin:1rem; cursor:pointer; object-fit:cover;">
+    <img src="${gelenimgUrl[i]}" class="card-img-top" alt="Kitty in the house " />
+    <div class="card-body">
+    <p class="card-text">ID:</p>
+    <p class="card-text">NAME :</p>
+    <p class="card-text">TYPE :</p>
+    <p class="card-text">ATK :</p>
+    <p class="card-text">DEF :</p>
+  </div>
+</div>`;
+  }
+  cardContainerShow.insertAdjacentHTML("beforeend", html);
+}
+
+yugiOhApiUse();
 
 //Fotoğraflara erişmek için bir yol
 
