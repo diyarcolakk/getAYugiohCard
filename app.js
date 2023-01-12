e762e76ee6fd4c64813c8976b1a78a2c4c135c9b
diyarcolakk <114312886+diyarcolakk@users.noreply.github.com>
2023-01-12 22:33:28 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
+"use strict";
 const yuGiOh = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+
 const cardContainerShow = document.querySelector(".card-container");
 const imgUrlBul = function (cardinfo) {
   let array = [];
   for (let i = 0; i < 20; i++) {
     array[i] = cardinfo.data[i].card_images[0].image_url;
   }
-
   return array;
 };
-
-function yerlestirme(gelenimgUrl) {
+const cardBilgileriBul = function (getTheCards) {
+  // Bir data arrayi oluşturdum.
+  let dataObjesi = {}; //-> Burada boş bir nesne oluşturdum.
+  dataObjesi = getTheCards
+  const { data: veri } = dataObjesi
+  let cardNesnesi = [{}]
+  for(let i= 0; i<100; i++) 
+  {
+    cardNesnesi[i] = {
+    id:`${veri[i].id}`,
+    name:`${veri[i].name}`,
+    race:`${veri[i].race}`,
+    type:`${veri[i].type}`,
+    url:`${veri[i].card_images[0].image_url}`,
+    }
+  }
+  console.log(cardNesnesi);
+  return cardNesnesi;
+}
+function yerlestirme(gelenCardNesnesi) {
   let html = "";
-  for (let i = 0; i < gelenimgUrl.length; i++) {
+  for (let i = 0; i < gelenCardNesnesi.length; i++) {
+  // console.log(gelenCardNesnesi[i].type);//"Spell Card"
+  //  if(gelenCardNesnesi[i].type === "Spell Card")
     html += `<div class="card" style="margin:1rem; cursor:pointer; object-fit:cover;">
-    <img src="${gelenimgUrl[i]}" class="card-img-top" alt="Kitty in the house " />
+    <img src="${gelenCardNesnesi[i].url}" class="card-img-top" alt="Kitty in the house " />
     <div class="card-body">
-    <p class="card-text">ID:</p>
-    <p class="card-text">NAME :</p>
-    <p class="card-text">TYPE :</p>
-    <p class="card-text">ATK :</p>
-    <p class="card-text">DEF :</p>
+    <p class="card-text">ID: ${gelenCardNesnesi[i].id}</p>
+    <p class="card-text">Name: ${gelenCardNesnesi[i].name}</p>
+    <p class="card-text">Race: ${gelenCardNesnesi[i].race}</p>
+    <p class="card-text">Type: ${gelenCardNesnesi[i].type}</p>
   </div>
 </div>`;
   }
   cardContainerShow.insertAdjacentHTML("beforeend", html);
+
 }
 
 const yugiOhApiUse = async function () {
   const useMyYuGiOh = await fetch(`${yuGiOh}`)
   const getTheCards = await useMyYuGiOh.json();
-  if (!getTheCards) throw new Error("Hata basıldı.")
-  yerlestirme(imgUrlBul(getTheCards))
+  yerlestirme(cardBilgileriBul(getTheCards))
 };
 yugiOhApiUse();
 
@@ -64,5 +84,3 @@ yugiOhApiUse();
 //       })
 //       .then((img) => console.log(img[1][0].image_url));
 //   };
-
-//   yugiOhApiUse();
